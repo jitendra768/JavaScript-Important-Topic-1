b28759d848540e5acd57c11a5f27d6c0b7fb3a5c
jitendra768 <jackjitendra768@gmail.com>
2022-06-10 21:51:44 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import stylesheets
 import './style.css';
 import './JavaScript/callbackAkshay'
+import './JavaScript/promiseAsyncAwait'
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
