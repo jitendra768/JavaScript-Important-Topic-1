24277735a292ba2f166cddb48842ba4a24668f2c
jitendra768 <jackjitendra768@gmail.com>
2022-06-11 02:35:05 +0530

    1 file modified

--- JavaScript/promiseAsyncAwait.js
+++ JavaScript/promiseAsyncAwait.js
@@ -7,4 +7,20 @@
 // // 4. get user data
 // // 5. Dispay user data
 
+let str = 'ABCDBC';
+let new_arr = [];
 
+// new_arr.push('amit');
+for (let index in str) {
+  if (new_arr.indexOf(str[index]) !== -1) {
+    for (var i = 0; i < new_arr.length; i++) {
+      if (new_arr[i] === str[index]) {
+        new_arr.splice(i, 1);
+        i--;
+      }
+    }
+  } else {
+    new_arr.push(str[index]);
+  }
+}
+console.log(new_arr[0]);
