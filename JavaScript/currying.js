dd900ed9a5845bfb0ec613fdf83013a626be86f5
jitendra768 <jackjitendra768@gmail.com>
2022-06-11 21:31:34 +0530

    1 file modified

--- JavaScript/currying.js
+++ JavaScript/currying.js
@@ -6,3 +6,14 @@ function addition(a,b,c){
 const res = addition(4,5,6);
 console.log(res);
 
+// in closure 
+
+function adddition(a){
+  return function(b){
+    return function(c){
+      return a+b+c;
+    }
+  }
+}
+let sum = adddition(2);
+console.log(result);
