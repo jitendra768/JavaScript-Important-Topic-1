512ba6d4e8ad17c720091122342b7f9f869a1d88
jitendra768 <jackjitendra768@gmail.com>
2022-06-11 21:53:50 +0530

    1 file modified

--- JavaScript/currying.js
+++ JavaScript/currying.js
@@ -1,19 +1,19 @@
-// Normal function use  
+// Normal function use
 
-function addition(a,b,c){
+function addition(a, b, c) {
   return a + b + c;
-};
-const res = addition(4,5,6);
+}
+const res = addition(4, 5, 6);
 console.log(res);
 
-// in closure 
+// in closure
 
-function adddition(a){
-  return function(b){
-    return function(c){
-      return a+b+c;
-    }
-  }
+function adddition(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
 }
 // const sum = adddition(4);
 // const data = sum(5);
@@ -25,17 +25,43 @@ function adddition(a){
 const sum = adddition(4)(5)(6);
 console.log(sum);
 
-
-// another Example 
+// another Example
 userObj = {
-  name:'Amit',
-  age:20
-}
+  name: 'Amit',
+  age: 20,
+};
 
-function userInfo(obj){
-  return function(userInfo){
+function userInfo(obj) {
+  return function (userInfo) {
     return obj[userInfo];
-  }
+  };
 }
 const user = userInfo(userObj);
 console.log(user('name'));
+
+// Geeks for Geeks example
+
+// function calculateVolume(length,breadth,height){
+//  return length * breadth *height;
+// }
+
+// console.log(calculateVolume(4,5,6));
+
+// with closure in currying
+function calculateVolume(length) {
+  return function (breadth) {
+    return function (height) {
+      return length * breadth * height;
+    };
+  };
+}
+console.log(calculateVolume(4)(5)(6));
+
+// Another Example help closure in currying function
+
+function product(x) {
+  return function (y) {
+    return x * y;
+  };
+}
+console.log(product(4)(5));
