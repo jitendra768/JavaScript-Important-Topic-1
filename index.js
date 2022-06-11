11880a7dc6182c9eaf6b6a5a700a33a91a2266b0
jitendra768 <jackjitendra768@gmail.com>
2022-06-11 21:56:28 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 import './style.css';
 // import './JavaScript/callbackAkshay';
 // import './JavaScript/promiseAsyncAwait';
-// import './JavaScript/const';
+import './JavaScript/const';
 // import './JavaScript/important';
-import './JavaScript/currying';
+// import './JavaScript/currying';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
