67f478c603c14d81699e6ffc004b0888dc19ffdf
jitendra768 <jackjitendra768@gmail.com>
2022-06-11 21:59:17 +0530

    1 file modified

--- JavaScript/const.js
+++ JavaScript/const.js
@@ -1,45 +1,54 @@
 // Changing the content of array is
 // possible in cost array
 
-const arr1 = ['pankaj', 'sumit', 'chandan', 'ajay'];
+// const arr1 = ['pankaj', 'sumit', 'chandan', 'ajay'];
 
-console.log(arr1.toString());
+// console.log(arr1.toString());
 
-arr1[2]='Amit';  //possible
-arr1.push('shubham');
+// arr1[2]='Amit';  //possible
+// arr1.push('shubham');
 
-console.log(arr1.toString());
+// console.log(arr1.toString());
 
 // It describes that the object properties can be modified only reference to object cannot be changed.
 
-const person = {
-  first_name: "Pankaj",
-  last_name: "Singh",
-  Age: 20,
-  About: "Web Developer and Competitive Programmer"
-};
+// const person = {
+//   first_name: "Pankaj",
+//   last_name: "Singh",
+//   Age: 20,
+//   About: "Web Developer and Competitive Programmer"
+// };
 
-console.log(person);
-person.first_name= 'Amit';
-person.Age= 26;
-person.last_name='kumar';
-console.log(person);
+// console.log(person);
+// person.first_name= 'Amit';
+// person.Age= 26;
+// person.last_name='kumar';
+// console.log(person);
 
 
 // It describes the const variable which contains the Block Scope.
 
-const x = 22;
-{
-    const x = 90;
-    console.log(x);
+// const x = 22;
+// {
+//     const x = 90;
+//     console.log(x);
 
-    {
-        const x = 77;
-        console.log(x);
-    }
-    {
-        const x = 45;
-        console.log(x);
-    }
-}
-console.log(x);
+//     {
+//         const x = 77;
+//         console.log(x);
+//     }
+//     {
+//         const x = 45;
+//         console.log(x);
+//     }
+// }
+// console.log(x);
+
+
+
+// Interview Question
+for(var i=0; i<10; i++){
+    setTimeout(()=>{
+        console.log(i)
+    });
+};
