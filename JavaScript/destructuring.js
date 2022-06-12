7c263ad48ffc3aba837fc3642de055b98319d890
jitendra768 <jackjitendra768@gmail.com>
2022-06-12 18:55:17 +0530

    1 file modified

---
+++ JavaScript/destructuring.js
@@ -0,0 +1,10 @@
+// Object Destructuring
+
+const users = {
+  name: 'Amit',
+  age: 87,
+  address: 'bhagalpur',
+};
+console.log(users);
+const { name, age, address } = users;
+console.log(name, age, address);
