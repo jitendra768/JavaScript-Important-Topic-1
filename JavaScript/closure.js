f784a9c19ee373914f39e01ff1e672694ea463bb
jitendra768 <jackjitendra768@gmail.com>
2022-06-12 15:07:11 +0530

    1 file modified

--- JavaScript/closure.js
+++ JavaScript/closure.js
@@ -1,21 +1,93 @@
-// function x() {
-//   var a = 7;
-//   function y() {
-//     console.log(a);
-//   }
-//   y();
+// closers in javascript
+
+// ---------example-----------------
+
+// var sum = function(a){
+//     console.log("live weievers" +a);
+//     var c=4;
+//     return function(b){
+//         return a+b+c;
+//     }
+// }
+
+// var store = sum(2);
+// console.log(store(5));
+
+
+// ---------------------example2-----------------------------
+
+// const outerfun= (a)=>{
+//     let b=10;
+//     const innerfun = ()=>{
+//         let sum = a+b;
+//         console.log(`the sum of two number ${sum}`)
+//     }
+//     // innerfun();
+//     return innerfun;
+// }
+// let inner = outerfun(5);
+// console.dir(inner);
+
+// const username = 'amit';
+
+// function login(){
+//     const password = 'secret';
+//     console.log(`${username} is loging in using ${password}`);
+// };
+// login();
+
+
+
+// ----------Akshay Saini -----------
+
+// function x(){
+//     var a = 7;
+//     function y(){
+//         console.log(a);
+//     }
+//     y();
 // }
 // x();
 
-// if return then what
+// as a parameter pass function
 
-function x() {
-  var a = 7;
-  function y() {
-    console.log(a);
-  }
-  return y;
+
+// function x(){
+//     var a = 7;
+    
+//     y();
+// }
+// x(function y(){
+//     console.log(a);
+// });
+
+
+// also return a function
+
+// function x(){
+//   var a = 7;
+//   function y(){
+//       console.log(a);
+//   }
+  // a = 100;   //given for confusion a interviewer  it will be hundred
+//   return y;
+// }
+// const z  = x();
+// console.log(z);
+// z();
+
+
+function z(){
+    var b = 20;
+    function x(){
+        var a = 3;
+        function y(){
+            console.log(a,b);
+        }
+        y();
+    }
+    x();
 }
-const z = x();
-console.log(z);
 z();
+
+
