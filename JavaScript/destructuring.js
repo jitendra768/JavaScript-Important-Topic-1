99810098303b7aa9ee062ed8e86eb253401b7069
jitendra768 <jackjitendra768@gmail.com>
2022-06-13 17:40:30 +0530

    1 file modified

--- JavaScript/destructuring.js
+++ JavaScript/destructuring.js
@@ -5,6 +5,16 @@ const users = {
   age: 87,
   address: 'bhagalpur',
 };
-console.log(users);
+// console.log(users);
 const { name, age, address } = users;
-console.log(name, age, address);
+// console.log(name, age, address);
+
+// -------------nested object destructuring---------------
+
+
+
+var count = 0;
+while (count<10){
+  console.log(count);
+  count++
+}
