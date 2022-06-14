c26fcc15c0f1b9bba590f93b10a96e44a4dd2dd5
jitendra768 <jackjitendra768@gmail.com>
2022-06-14 15:37:31 +0530

    1 file modified

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -1,12 +1,39 @@
 // function with CoderGyan
 
-function login(userName, password){
-  console.log(`${userName} login successfull`)
-};
+function login(userName, password) {
+  console.log(`${userName} login successfull`);
+}
 // login('Amit')
 
-function upperCase(str){
+function upperCase(str) {
   return str.toUpperCase();
 }
 const upper = upperCase('javascript');
-console.log(upper);
+// console.log(upper);
+
+// how to js work
+
+var n = 2;
+function square(num) {
+  return num * num;
+}
+const square1 = square(n);
+const square2 = square(4);
+// console.log(square1);
+// console.log(square2);
+
+// how to function work in js
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a() {
+  var x = 10;
+  console.log(x);
+}
+function b() {
+  var x = 100;
+  console.log(x);
+}
