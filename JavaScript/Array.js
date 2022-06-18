63ab305aa24b7b9c940838a140044eeb98f240bd
jitendra768 <jackjitendra768@gmail.com>
2022-06-18 14:58:16 +0530

    1 file modified, 1 added

---
+++ JavaScript/Array.js
@@ -0,0 +1,17 @@
+// 'fruits' array created using array literal notation.
+
+// const fruits= ['Apple', 'Banana'];
+// console.log(fruits);
+// console.log(fruits.length);
+
+// 'fruits' array created using the Array() constructor.
+
+// const fruits = new Array('Apple', 'Banana');
+// console.log(fruits);
+// console.log(fruits.length);
+
+// 'fruits' array created using String.prototype.split().
+
+const fruits = 'Apple, Banana'.split(',');
+console.log(fruits);
+console.log(fruits.length);
