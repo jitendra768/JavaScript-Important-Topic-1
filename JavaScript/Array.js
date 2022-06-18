bde097350aab9a7eca238d23d2ba461528b19934
jitendra768 <jackjitendra768@gmail.com>
2022-06-18 15:38:26 +0530

    1 file modified

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -16,26 +16,22 @@
 // console.log(fruits);
 // console.log(fruits.length);
 
-
 // This example uses the join() method to create a string from the fruits array.
 
 // const fruits = ['Apple', 'Banana'];
 // const fruitsString = fruits.join(',');
 // console.log(fruitsString);
 
-
 // This example uses the indexOf() method to find the position (index) of the string "Banana" in the fruits array.
 
 // const fruits = ['Apple', 'Banana'];
 // console.log(fruits.indexOf('Banana'));
-
 
 // This example shows two ways to check if the fruits array contains "Banana" and "Cherry": first with the includes() method, and then with the indexOf() method to test for an index value that's not -1.
 
 // const fruits = ['Apple','Banana'];
 // console.log(fruits.includes('Banana')); // true
 // console.log(fruits.includes('Cherry')); // false
-
 
 // This example uses the push() method to append a new string to the fruits array.
 
@@ -44,11 +40,19 @@
 // console.warn(fruits);
 // console.log(newFruits);
 
-
 // Remove the last item from an array
 // This example uses the pop() method to remove the last item from the fruits array.
 
-const fruits = ['Apple','Banana', 'Cherry'];
-const removeItem = fruits.pop();
+// const fruits = ['Apple','Banana', 'Cherry'];
+// const removeItem = fruits.pop();
+// console.log(fruits);
+// console.log(removeItem);
+
+// Remove multiple items from the end of an array
+// This example uses the splice() method to remove the last 3 items from the fruits array.
+
+const fruits = ['Apple', 'Banana', 'Cherry', 'Strawberry', 'Mango'];
+const start = -3;
+const removeItem = fruits.splice(start);
 console.log(fruits);
 console.log(removeItem);
