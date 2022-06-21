a1a19bd95113a78c723544adb5b789abbfee6c36
jitendra768 <jackjitendra768@gmail.com>
2022-06-21 12:23:37 +0530

    1 file modified

--- JavaScript/scopeChain&LexicalScope.js
+++ JavaScript/scopeChain&LexicalScope.js
@@ -1,4 +1,4 @@
-// What exactly is Scope?
+//1 What exactly is Scope?
 
 // Define a variable in the global scope:
 const fullName = 'Oluwatobi Sofela';
@@ -16,3 +16,22 @@ function profile() {
 
 const result = profile();
 console.log(result);
+
+// if we have define three fullName
+
+const FullName = 'Shubham kumar';
+
+function details(){
+const FullName = 'Amit kumar';
+
+  function name(){
+const FullName = 'Sachin shahani';
+
+    function anotherName(){
+      return FullName;
+    }
+    anotherName();
+  }
+  name();
+}
+console.log(details());
