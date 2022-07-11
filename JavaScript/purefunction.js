631abaacca3be7a1e7ea92b4a789b4929383e29a
jitendra768 <jackjitendra768@gmail.com>
2022-07-11 10:33:22 +0530

    1 file modified, 1 added

---
+++ JavaScript/purefunction.js
@@ -0,0 +1,17 @@
+// this is puredunction 
+
+function sum(a,b){
+  return a+b;
+}
+console.log(sum(5,3));
+console.log(sum(5,3));
+
+// impure function 
+
+let a = 5;
+
+function some(arg){
+  return a+2
+};
+
+console.log(some())
